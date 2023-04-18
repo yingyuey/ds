8a1ea3d701fa4fc25b782cd6b0c52e028e3c54e4
yingyuey <128228346+yingyuey@users.noreply.github.com>
2023-04-18 23:23:35 +0800

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,7 @@
+#include"../../List/List.h"
+ template <typename T> class Queue : public List<T> { //队列模板类
+ public: //原有接口一概沿用
+    void enqueue(T const& e) { insertAsLast(e); } //入队：尾部插入
+    T dequeue() { return remove(first()); } //出队：首部删除
+    T & front() { return first()->data; } //队首
+};
